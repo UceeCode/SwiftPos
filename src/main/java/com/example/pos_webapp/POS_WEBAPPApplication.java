635d52569d7edcb80e6968758e8e83da2a0f3a37
Franklin <writefranklyn@gmail.com>
2024-09-23 12:00:50 +0100

    init project files

---
+++ src/main/java/com/example/pos_webapp/POS_WEBAPPApplication.java
@@ -0,0 +1,13 @@
+package com.example.pos_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class POS_WEBAPPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(POS_WEBAPPApplication.class, args);
+    }
+
+}

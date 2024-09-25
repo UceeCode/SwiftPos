f22bfd09ce95732ff1cb1b8ea83ae936a4f0aee2
Franklin <writefranklyn@gmail.com>
2024-09-25 11:35:04 +0100

    add admin request and response

--- src/main/java/com/example/pos_webapp/Dto/AdminRequest.java
+++ src/main/java/com/example/pos_webapp/Dto/AdminRequest.java
@@ -1,4 +1,17 @@
 package com.example.pos_webapp.Dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminRequest {
+    private String fullname;
+    private String email;
+    private String phoneno;
+    private String username;
+    private String password;
 }
--- src/main/java/com/example/pos_webapp/Dto/AdminResponse.java
+++ src/main/java/com/example/pos_webapp/Dto/AdminResponse.java
@@ -1,4 +1,12 @@
 package com.example.pos_webapp.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminResponse {
+    private String username;
 }
